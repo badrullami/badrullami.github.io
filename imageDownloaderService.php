7481e9cfbfb143d72c4f166ff80e19a5627906bb
Mohammad Badrullami <mohammad.badrullami@gmail.com>
2015-03-15 21:36:25 +0700

    autoupdate 2015-03-15-21:36:25

--- imageDownloaderService.php
+++ imageDownloaderService.php
@@ -95,7 +95,7 @@
 				endif;
 
 				/* get original binary */
-				smart_resize_image(null , file_get_contents($row['Original']), $row['Original_Width'] , $row['Original_Height'], false , $path_original, false , false ,100);
+				// smart_resize_image(null , file_get_contents($row['Original']), $row['Original_Width'] , $row['Original_Height'], false , $path_original, false , false ,100);
 
 				mysql_query("update images set 
 					Thumbnail ='".$url.$path_thumbnail."', 
